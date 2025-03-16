fa7b1a6fe9c28fb966dfcd33c49ac73f44f1884f
DataNinja10 <163555542+DataNinja10@users.noreply.github.com>
2025-03-16 14:18:38 +0100

    Agregar archivos de proyecto.

---
+++ Page Navigation App/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Page_Navigation_App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool isDragging = false;
+        private Point lastMousePosition;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Frame_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            isDragging = true;
+            lastMousePosition = e.GetPosition(this);
+            (sender as Frame).CaptureMouse();
+        }
+
+        private void Frame_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                Point currentPosition = e.GetPosition(this);
+                double offsetX = currentPosition.X - lastMousePosition.X;
+                double offsetY = currentPosition.Y - lastMousePosition.Y;
+
+                this.Left += offsetX;
+                this.Top += offsetY;
+            }
+        }
+
+        private void Frame_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            isDragging = false;
+            (sender as Frame).ReleaseMouseCapture();
+        }
+    
+
+        private void CloseApp_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Btn_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Page Navigation App/View/Products.xaml.cs
@@ -0,0 +1,41 @@
+﻿using CloudyApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Page_Navigation_App.View
+{
+    /// <summary>
+    /// Interaction logic for Products.xaml
+    /// </summary>
+    public partial class Products : UserControl
+    {
+        public Products()
+        {
+            InitializeComponent();
+        }
+
+        private async void aisend_Click(object sender, RoutedEventArgs e)
+        {
+            string res = await Api.AskAi(TextBox.Text);
+            res = Api.Cleanres(res);
+            TextBox.Text = res;
+        }
+
+        private void copy_Click(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(TextBox.Text);
+        }
+    }
+}
